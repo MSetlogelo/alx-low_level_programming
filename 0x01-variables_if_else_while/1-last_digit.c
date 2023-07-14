88f8fcfa16b5f6b01551fff3df8e594de46d3f4f
Mmasetlogelo Setlogelo <mosetlogelo@gmail.com>
2023-07-14 17:42:23 +0200

    Git commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,28 +11,22 @@
 int main(void)
 {
 	int n;
-	int lDigit;
-
 srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lDigit = n % 10;
-
-	printf("The string Last digit of %d is", n);
-
-if (lDigit > 5)
+if (n > 5)
 {
-	printf(" and is greater than 5\n", n);
+	printf(" %d and is greater than 5\n", n);
 }
 
-	else if (lDigit == 0)
+	else if (n < 6 || n != 0)
 {
 
-	printf(" and is 0\n", n);
+	printf(" %d and is less than 6 and not 0\n", n);
 }
 
 	else
 {
-	printf(" and is less than 6 and not 0\n", n);
+	printf(" %d is %d and is 0\n", n);
 }
 
 	return (0);
