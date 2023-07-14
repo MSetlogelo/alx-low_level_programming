d66fbb9fac501dd64d51160a35c0855046718233
Mmasetlogelo Setlogelo <mosetlogelo@gmail.com>
2023-07-14 20:50:14 +0200

    commiting using alias

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c,h;
+
+	for (c = 0; c < 9; c++)
+{
+	for(h = c + 1; h < 10; h++)
+{	
+	putchar('0' + c);
+	putchar('0' + h);
+	if (c !=8 || h != 9)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+}
+	putchar('\n');
+	return (0);
+}
