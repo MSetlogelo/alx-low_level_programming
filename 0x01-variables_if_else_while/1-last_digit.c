56e457f58c3f15441fb8cf2c87e412fc055eb64c
Mmasetlogelo Setlogelo <mosetlogelo@gmail.com>
2023-07-14 18:22:45 +0200

    commiting using alias

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,22 +15,22 @@ int main(void)
 	int last#;
 srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last# = n % 10;
+	lastDigit = n % 10;
 
-if (last# > 5)
+if (lastDigit > 5)
 {
-	printf("Last digit of %d is %d and is greater than 5\n", n, last#);
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
 
-	else if (n < 6 || n != 0)
+	else if (lastDigit < 6 || n != 0)
 {
 
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last#);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 }
 
 	else
 {
-	printf("Last digit of %d is %d and is 0\n", n, last#);
+	printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 }
 
 	return (0);
