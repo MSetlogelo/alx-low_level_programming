e89b050602ff35f3e5199cea5700fe9ac321221d
Mmasetlogelo Setlogelo <mosetlogelo@gmail.com>
2023-07-14 19:04:02 +0200

    commiting using alias

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+{
+	putchar(ch);
+}
+	for (ab = 'A'; ab <= 'Z'; ab++)
+{
+	putchar(ab);
+}
+	putchar('\n');
+	return (0);
+}
