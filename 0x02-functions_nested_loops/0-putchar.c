428f4dcf2b87eac27b48a7f2c00983b2841447f0
Mmasetlogelo Setlogelo <mosetlogelo@gmail.com>
2023-07-18 20:45:52 +0200

    Commiting using an alias for all tasks

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,9 +6,17 @@
  * Return: Always 0 (Success)
  */
 /* betty style doc for function main goes there */
-	int main(void)
-{
-	_purchar("\_putchar\n");
 
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
