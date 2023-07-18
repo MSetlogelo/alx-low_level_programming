13be48d53ba8cfe18c7ab27bc13ea52d58b214e0
Mmasetlogelo Setlogelo <mosetlogelo@gmail.com>
2023-07-18 21:16:44 +0200

    Commiting using an alias for all tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+include "main.h"
+/**
+* print_alphabet - Prints the alphabet in lowercase
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+	int c;
+
+	for(c =97; c <= 122; c++)
+	{
+		_puntchar(c);
+	}
+	_putchar('\n');
+}
