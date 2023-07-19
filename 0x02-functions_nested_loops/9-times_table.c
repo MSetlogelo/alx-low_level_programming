28bf221410db4d0732cf977870f5d969b6dfa598
Mmasetlogelo Setlogelo <mosetlogelo@gmail.com>
2023-07-19 19:06:31 +0200

    commiting before second deadline

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,23 +16,23 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			product = i * j;
-			if (j == 0)
-			{
-				_putchar('0');
-			}
-			else
+			if (j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-
-			if (product < 10)
+			if (product < 10 && j != 0)
 			{
 				_putchar(' ');
 			}
-			_putchar(product + '0');
+
+			if (product >= 10)
+			{
+				_putchar(product / 10 + '0');
+			}
+			_putchar(product % 10 + '0');
 		}
-	}
 	_putchar('$');
 	_putchar('\n');
+	}
 }
