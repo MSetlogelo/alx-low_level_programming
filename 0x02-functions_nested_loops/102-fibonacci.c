44f8a2a17031fa34fbdbf7e7630e05730b1f39d1
Mmasetlogelo Setlogelo <mosetlogelo@gmail.com>
2023-07-19 22:19:22 +0200

    commiting before second deadline

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Print the first 50 Fibonacci numbers, from 1 and 2
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int c, h, a, r;
+
+	h = 1;
+
+	a = 2;
+
+	for (c = 1; c <= 50; c++)
+	{
+		if (h != 20365011074)
+		{
+			prinf("%ld", h);
+		}
+		else
+		{
+			printf("%ld\n", h);
+		}
+		r = h + a;
+		h = a;
+		a = r;
+	}
+	return (0);
+}
