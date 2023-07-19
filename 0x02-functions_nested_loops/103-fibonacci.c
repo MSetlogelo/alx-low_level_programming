335d5454afdb258563cd0874a5133fe8e674099d
Mmasetlogelo Setlogelo <mosetlogelo@gmail.com>
2023-07-19 22:32:50 +0200

    commiting before second deadline

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - print the sum of the even numbers
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+	unsigned long int h, a, r, n, sum;
+
+	h = 1;
+	a = 2;
+	sum = 0;
+
+	for (c = 1; c <= 33; ++c)
+	{
+		if (h < 4000000 && (h % 2) == 0)
+		{
+			sum = sum + h;
+		}
+
+		n = h + a;
+		j = a;
+		a = n;
+	}
+	printf("%lu\n", sum);
+
+	return (0);
+
+}
