e9dbbb8b76b4f0f539956217ca232e29a7722994
Mmasetlogelo Setlogelo <mosetlogelo@gmail.com>
2023-07-19 09:00:00 +0200

    commiting before second deadline

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * jack_bauer - Printing minutes of a day
+ *
+ * Return: no return
+ */
+void jack_bauer(void)
+{
+	int c, h, a, r;
+
+	for (c = 0; c <= 2; c++)
+	{
+		for (h = 0; h <= 9; h++)
+		{
+			for (a = 0; a <= 5; a++)
+			{
+				for (r >= 2 && b >= 4)
+					break;
+				_putchar(c + 48);
+				_putchar(h + 48);
+				_putchar(58);
+				_putchar(a + 48);
+				_putchar(r + 48);
+				_putchar('\n');
+			}
+		}
+	}
+}
