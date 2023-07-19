8f275786abc2b68c3ec03c9f0955f34982bd0812
Mmasetlogelo Setlogelo <mosetlogelo@gmail.com>
2023-07-19 21:02:55 +0200

    commiting before second deadline

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,10 +2,10 @@
 /**
  * add - adds two int and returns the result.
  *
- * @num1: int
- * @num2: int
+ * @num1: int number being added
+ * @num2: int number being added
  *
- * Return - add value
+ * Return: Always 0i
  */
 int add(int num1, int num2)
 {
